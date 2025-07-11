7e141f6ebd6dd405a6161257c33f7d78e65e3802
cristatechnologies <tarak.thakarar@gmail.com>
2025-07-11 16:16:14 +0530

    added the google meta tag

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
 "use client";
 
+import Head from "next/head";
 import HomePage from "@/components/HomePage/page";
 import PageHead from "@/components/helper/PageHead/page";
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="6PMtHZQCYmr6ZqDTumqj-lyyjJ2B_mHrk14TnvfOfKU"
+        />
+      </Head>
       <PageHead title="Active Deals" metaDes="Active Deals" />
-
       <HomePage />
     </>
   );
